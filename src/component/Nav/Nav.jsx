a11d121e73a15af3888fa135e342260a8239de08
Wild-Design <brenneke_ruger@hotmail.com>
2023-02-05 21:22:40 -0300

    Modo serio activado

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -1,48 +1,91 @@
-import { Flex, Divider, Button} from "@chakra-ui/react";
-import { MdBuild} from "react-icons/md"
+import { Flex, Button, Text } from "@chakra-ui/react";
+import { MdBuild } from "react-icons/md";
 import { useSelector } from "react-redux";
 import ChatsList from "../ChatsList/ChatsList.jsx";
-import LogoutButton from "../LogoutButton/LogoutButton.jsx";
 import Profile from "../Profile/Profile.jsx";
+import { errorExit } from "../../services/sweetalert.js";
+import { sendMessage } from "../../services/sockets";
+import { BsBoxArrowInRight } from "react-icons/bs";
+import { useAuth0 } from "@auth0/auth0-react";
 
+export default function Nav({ handle }) {
+  const { user, logout } = useAuth0();
+  const { my } = useSelector((state) => state.users);
+  const handler = () => {
+    errorExit().then((response) => {
+      if (response) {
+        sendMessage("exit", user);
+        logout({
+          returnTo: "https://client-deploy-wild-design.vercel.app/",
+          client_id: "WawCbbdYBrnbIDvqnhUIyulgHhicovQJ",
+        });
+      }
+    });
+  };
 
-export default function Nav({handle}) {
-
-  const {my} =  useSelector(state=>state.users)
   return (
     <Flex
       justifyContent="flex-start"
-      minW="18%"
+      flexDirection={"column"}
+      minW="20px"
       bg="#232223"
       direction="column"
-      gap="3" 
+      gap="3"
     >
-
-      <Flex direction={{base:"column", lg:"row"}} p="3px" justify="space-between" alignItems="center"> 
+      <Flex
+        direction={{ base: "column", lg: "row" }}
+        p="3px"
+        mb="20px"
+        justify="space-between"
+        alignItems="center"
+      >
         <Profile />
-        {my.type==='admin'&&<Flex>
-        <Button 
-        leftIcon={<MdBuild />} 
-        colorScheme='gray' 
-        color="white"
-        _hover={{color:"black", bg:"white"}}
-        variant='outline' 
-        size='sm'
-        mt="20px"
-        onClick={()=>{handle(true)}}>
-          Admin
-          </Button>
-       </Flex>}
+        {my.type === "admin" && (
+          <Flex>
+            <Button
+              leftIcon={<MdBuild />}
+              colorScheme="gray"
+              color="white"
+              _hover={{ color: "black", bg: "white" }}
+              variant="outline"
+              size="sm"
+              mt="10px"
+              mr="10px"
+              onClick={() => {
+                handle(true);
+              }}
+            >
+              Admin
+            </Button>
+          </Flex>
+        )}
       </Flex>
 
-    <Divider mt="-7px" mb="6px"/>
-      
-
+      <Flex justify="center">
+        <Text as="b" color="white">
+          {" "}
+          Chats Activos{" "}
+        </Text>
+      </Flex>
 
       <Flex mx="2">
         <ChatsList />
       </Flex>
-       
+
+      {/* <Flex justifyContent={"center"} bg="red">
+        <Button
+          onClick={handler}
+          bg="#FF4e5b"
+          _hover={{ bg: "red.500" }}
+          rightIcon={<BsBoxArrowInRight />}
+          colorScheme="white"
+          variant="solid"
+          position="absolute"
+          bottom="16"
+        >
+          Cerrar Sesión
+        </Button>
+      </Flex> */}
     </Flex>
   );
 }

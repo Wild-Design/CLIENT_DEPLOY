d12b500fc152edd1801f42889c1013a7d33fd4ac
Wild-Design <brenneke_ruger@hotmail.com>
2023-03-17 15:33:37 -0300

    configuro coneccion ala coso nueva

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ import Faq from "./component/LandingPageRework/Faq";
 import Blacklist from "./component/BlackList/BlackList.jsx";
 import axios from "axios";
 // axios.defaults.baseURL ='http://localhost:3001/';//// cambiar a ruta del back end
-axios.defaults.baseURL = "https://serverdeploy-production.up.railway.app/"; //// cambiar a ruta del back end
+axios.defaults.baseURL = "https://talkap.com.ar/api/"; //// cambiar a ruta del back end
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Routes>
-        <Route path="/" element={<LandingPageRework />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/aboutUs" element={<AboutUs />} />
-        <Route path="/faq" element={<Faq />} />
-        <Route path="/blacklist" element={<Blacklist />} />
+        <Route path='/' element={<LandingPageRework />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/aboutUs' element={<AboutUs />} />
+        <Route path='/faq' element={<Faq />} />
+        <Route path='/blacklist' element={<Blacklist />} />
       </Routes>
     </div>
   );
